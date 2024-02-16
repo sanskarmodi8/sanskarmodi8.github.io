ef200bdc0dfe2ef8efdf42dc01098fb5c79de44e
Sanskar Modi <sansyprog8@gmail.com>
2024-02-16 18:45:04 +0530

    updated project card gradient

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -544,20 +544,20 @@ const Project = ({ name, url, img, repourl }) => {
         <VStack
           pos={'absolute'}
           borderRadius={'10px'}
-          _hover={{ opacity: '0.95' , background : 'linear-gradient(90.21deg, #AA367C -5.91%, #4A2FBD 111.58%)'}}
+          _hover={{ opacity: '0.95' , background : 'linear-gradient(90.21deg, #9370db -5.91%, #9370db 111.58%)'}}
           top={0}
           bottom={0}
           left={0}
           right={0}
-          opacity={0.8}
+          opacity={0.7}
           transition={'0.3s ease-in-out'}
           justifyContent={'center'}
           alignItems={'center'}
           background={
-            'linear-gradient(90.21deg, #AA367C -5.91%, #4A2FBD 111.58%)'
+            'linear-gradient(90.21deg, #9370db -5.91%, #9370db 111.58%)'
           }
         >
-          <Text p={'5px'} fontSize={"2xl"} color={'white'} as={'h2'} fontWeight={"extrabold"}>{`${name}`}</Text>
+          <Text p={'5px'} fontSize={"xl"} color={'white'} as={'h2'} fontWeight={"extrabold"}>{`${name}`}</Text>
           <Box display={'flex'}>
             {url!=="" ? (<a href={url} target="blank">
             
